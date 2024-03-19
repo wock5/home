701246ff4ee51c95cf2800e1de5cdd8bc5dc1aae
DH <wwasdasd125@gmail.com>
2024-03-19 16:25:38 +0900

    예습

---
+++ src/Study.java
@@ -0,0 +1,9 @@
+public class Study {
+    public static void main(String[] args) {
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+    }
+}
