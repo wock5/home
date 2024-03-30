3090d4734007b2c98ca90260b7fc5eb97a2ea2c2
DH <wwasdasd125@gmail.com>
2024-03-30 16:31:43 +0900

    이스케이프 문자열

---
+++ src/Study07.java
@@ -0,0 +1,9 @@
+public class Study07 {
+    public static void main(String[] args) {
+        System.out.println("\n줄바꿈\n연습");
+        System.out.println("\t탭키\t연습");
+        System.out.println("어떤 글자를 \"강조\"하는효과");
+        System.out.println("어떤 글자를 \'강조\'하는효과");
+        System.out.println("\\\\백슬래시 2개출력");
+    }
+}
