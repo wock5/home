2182fc7208c002180b04088a3ffc733df3abe990
DH <wwasdasd125@gmail.com>
2024-03-30 16:19:07 +0900

    indexOf 문자열찾기 substring 글자 뽑기

---
+++ src/Study06.java
@@ -0,0 +1,12 @@
+public class Study06 {
+    public static void main(String[] args) {
+        String ind = "난생처음 자바를 처음 학습 중입니다. 자바는 처음이지만 재미있네요.";
+        String str = "Java";
+
+        System.out.println(ind.indexOf("처음"));
+        System.out.println(ind.indexOf("학습"));
+        System.out.println(ind.indexOf("처음",4));
+        System.out.println(ind.indexOf("자바",10));
+        System.out.println(str.substring(2,3));
+    }
+}
