98497a5c80b38ee6341ae14d85df72b75b6479f5
DH <wwasdasd125@gmail.com>
2024-03-31 17:09:27 +0900

    문자열 찾기 문자열 뽑기 소스 혼자 만들어보기

---
+++ src/Study08.java
@@ -0,0 +1,8 @@
+public class Study08 {
+    public static void main(String[] args) {
+        String ido = "소스만들기를 집에서 혼자 연습하는 중입니다";
+
+        System.out.println(ido.indexOf("집에서"));
+        System.out.println(ido.substring(7,10));
+    }
+}
