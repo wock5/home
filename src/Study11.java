b09fa9ec8b91358423315a6dbf1e02388a8b2cc7
DH <wwasdasd125@gmail.com>
2024-04-07 21:14:58 +0900

    printf 활용, 서식

---
+++ src/Study11.java
@@ -0,0 +1,13 @@
+public class Study11 {
+    public static void main(String[] args) {
+        System.out.printf("200+300");
+        System.out.println();
+        System.out.printf("%d", 200+300);
+        System.out.println();
+        System.out.printf("%d", 123);
+        System.out.println();
+        System.out.printf("%5d", 123);
+        System.out.println();
+        System.out.printf("%05d", 123);
+    }
+}
