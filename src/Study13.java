ab6c604587e820fe5baf396b0087cab1deb3c584
DH <wwasdasd125@gmail.com>
2024-04-10 00:21:27 +0900

    대입 연산자

---
+++ src/Study13.java
@@ -0,0 +1,21 @@
+public class Study13 {
+    public static void main(String[] args) {
+        //대입 연산자
+        int num = 10;
+        num = num + 2;
+        System.out.println(num); //12
+
+        num = num - 2;
+        System.out.println(num); //10
+
+        //복합 대입 연산자
+        num = 10;
+        //num = num + 2;
+        num += 2;
+        System.out.println(num); //12
+
+        // num = num - 2 ;
+        num -= 2;
+        System.out.println(num); //10
+    }
+}
