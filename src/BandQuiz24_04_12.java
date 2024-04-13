0a1c9d5a860d2bf1bec914bca76a53234c2e1675
DH <wwasdasd125@gmail.com>
2024-04-13 22:09:08 +0900

    소스 업로드

---
+++ src/BandQuiz24_04_12.java
@@ -0,0 +1,11 @@
+public class BandQuiz24_04_12 {
+    public static void main(String[] args) {
+        int hap = 0;
+        for (int i=201; i<=300 ; i+=2) {
+            hap += i;
+        }
+        System.out.println("200부터 300까지의 홀수의 합계 : " + hap);
+    }
+}
+
+
