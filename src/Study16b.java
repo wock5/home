f1a331610cc9ffc83cb4dd8539262d3f485cbc5c
DH <wwasdasd125@gmail.com>
2024-05-15 23:12:15 +0900

    구구단 만들기

--- src/Study16b.java
+++ src/Study16b.java
@@ -1,6 +1,6 @@
 public class Study16b {
     public static void main(String[] args) {
-        for (int i = 2; i <=9  ; i++) {
+        for (int i = 3; i <=9  ; i++) {
             for (int j = 1; j <=9 ; j++) {
                 System.out.println(i + " * " + j + " = " + i*j);
             }
